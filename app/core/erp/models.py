c554aaeb75a4bbe80270e76eb55c4f387a196d40
Diana Cadena <diana.marcela.cadena@correounivalle.edu.co>
2024-08-16 14:05:36 -0500

    models PARTE II

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,7 +1,33 @@
 from django.db import models
 from datetime import datetime
 
+class Type(models.Model):
+    names = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.names
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        db_table = 'tipo'
+        ordering = ['id']
+
+class Category(models.Model):
+    names = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.names
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        db_table = 'categoria'
+        ordering = ['id']
+
 class Employee(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    categoria = models.ManyToManyField(Category)
     names = models.CharField(max_length=150, verbose_name='Nombres')
     dni = models.CharField(max_length=10, unique=True, verbose_name='Dni')
     data_joined = models.DateField(default=datetime.now, verbose_name='Fecha de registro')
